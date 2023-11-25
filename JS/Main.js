64327b39c49c3d6f0986cf411595082cf65f1edb
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-25 13:10:48 +0330

    feat: develop AddParagraph function and event listener

--- JS/Main.js
+++ JS/Main.js
@@ -23,3 +23,19 @@ let ENParagraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
 let ENSentence = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." , "Egestas purus viverra accumsan in nisl nisi." , "Arcu cursus vitae congue mauris rhoncus aenean vel elit sceleirsque." , "In egestas erat imperdiet sed euismod nisi porta lorem mollis." , "Morbi tristique senectus et netus." , "Mattis pellentesque id nibh tortor id aliquet lectus proin." , "Sapien faucibus et molestie ac feugiat sed lectus vestibulum." , "Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget." , "Dictum varius duis at consectetur lorem." , "Nisi vitae suscipit tellus mauris a diam maecenas sed enim." , "Velit ut tortor pretium viverra suspendisse potenti nullam." , "Et molestie ac feugiat sed lectus." , "Non nisi est sit amet facilisis magna." , "Dignissim diam quis enim lobortis scelerisque fermentum." , "Odio ut enim blandit volutpat maecenas volutpat." , "Ornare lectus sit amet est placerat in egestas erat." , "Nisi vitae suscipit tellus mauris a diam maecenas sed." , " Placerat duis ultricies lacus sed turpis tincidunt id aliquet."];
 
 let ENWord = ["Lorem" , "ipsum" ,"dolor" ,"sit" ,"amet" ,"consectetur" ,"adipiscing" ,"elit" ,"sed" ,"do" ,"eiusmod" ,"tempor" ,"incididunt" ,"ut" ,"labore" ,"et" ,"dolore" ,"magna" ,"aliqua" ,"Egestas" ,"purus" ,"viverra" ,"accumsan" ,"in" ,"nisl" ,"nisi" ,"Arcu" ,"cursus" ,"vitae" ,"congue" ,"mauris" ,"rhoncus" ,"aenean" ,"vel" ,"elit" ,"sceleirsque" ,"In" ,"egestas" ,"erat" ,"imperdiet" ,"sed" ,"euismod" ,"nisi" ,"porta" ,"lorem" ,"mollis" ,"Morbi" ,"tristique" ,"senectus" ,"et" ,"tincidunt" ,"ornare" ,"massa" ,"eget" ,"Dictum" ,"varius" ,"duis" ,"at" ,"consectetur" ,"lorem" ,"Nisi" ,"vitae" ,"suscipit" ,"tellus" ,"mauris" ,"a" ,"diam" ,"maecenas" ,"sed" ,"enim" ,"Velit" ,"ut" ,"tortor" ,"pretium" ,"viverra" ,"suspendisse" ,"potenti" ,"nullam" ,"Et" ,"molestie" ,"ac" ,"feugiat" ,"sed" ,"lectus" ,"Non" ,"nisi" ,"sit" ,"amet" ,"facilisis" ,"magna" ,"Dignissim" ,"diam" ,"quis" ,"enim" ,"lobortis" ,"scelerisque" ,"fermentum" ,"Odio" ,"ut" ,"enim" ,"blandit" ,"volutpat" ,"maecenas" ,"volutpat" ,"Ornare" ,"lectus" ,"sit" ,"amet" ,"est" ,"placerat" ,"in" ,"egestas" ,"erat" ,"Nisi" ,"vitae" ,"suscipit" ,"tellus" ,"mauris" ,"a" ,"diam" ,"maecenas" ,"sed" ,"Placerat" ,"duis" ,"ultricies" ,"lacus" ,"sed" ,"turpis" ,"tincidunt" ,"id" ,"aliquet"]
+
+
+function AddParagraph(){
+  for(let i = 0; i < CountGenerate.value ; i++){
+     if(PersionSelect.checked){
+       GeneratedTxt.innerHTML += `<br> ${FAParagraph}`;
+     }else if(EnglishSelect.checked){
+      GeneratedTxt.innerHTML += `<br> ${ENParagraph}`;
+     }
+  }
+}
+
+//EventListener
+PersionSelect.addEventListener('change' , (e)=>{
+  AddParagraph();
+});
